e84fab1a5d7eddfd17b3dc01e664072090d40f08
Ben Kurose <ben.kurose@gmail.com>
2022-08-23 14:04:29 -0400

    loads information into navigation bar, CSS updates

--- index.js
+++ index.js
@@ -3,24 +3,26 @@ fetch('https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_tran
 .then(res => {
     let disclosures = (res.filter(item => item['amount'] == '$5,000,001 - $25,000,000' || item['amount'] == '$25,000,001 - $50,000,000' || item['amount'] == '$50,000,000 +'))
     console.log(disclosures)
-    // disclosures.forEach(item => renderNavItem(item))
+    disclosures.forEach(item => renderNavItem(item))
 })
 
-// function renderNavItem(item){
-//     let navBar = document.querySelector('#navigationBar')
-//     let container = document.createElement('div')
-//     container.className = 'navItem'
-//     let date = document.createElement('h3')
-//     let member = document.createElement('h4')
-//     let ticker = document.createElement('h4')
+function renderNavItem(item){
+    let navBar = document.querySelector('#navigationBar')
+    let container = document.createElement('div')
+    container.className = 'navItem'
+    let date = document.createElement('h3')
+    let member = document.createElement('h4')
+    let ticker = document.createElement('h4')
 
-//     date.textContent = `Date: ${item.transaction_date}`
-//     member.textContent = `${item.representative}`
-//     ticker.textContent = `${item.ticker}`
+    date.textContent = `Date: ${item.transaction_date}`
+    member.textContent = `${item.representative}`
 
-//     container.append(date, member,ticker)
-//     navBar.append(container)
-// }
+    if(item.ticker == '--'){ticker.textContent = `Not Publicly Traded`}
+        else{ticker.textContent = `${item.ticker}`}
+
+    container.append(date, member,ticker)
+    navBar.append(container)
+}
 
 
 
