fddc6360fd6cf920eee030dfd71c4547a7c86ffe
Ben Kurose <ben.kurose@gmail.com>
2022-08-23 12:30:04 -0400

    created HTML skeleton, refined JS object filter

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 fetch('https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_transactions.json')
 .then(res => res.json())
-.then(res => res.forEach((item)=> {
-    if(item['amount'].includes('$1,000,001 - $5,000,000') == true){
-        console.log(item)
-    }
-}))
+.then(res => {
+    let disclosures = (res.filter(item => item['amount'] == '$5,000,001 - $25,000,000' || item['amount'] == '$25,000,001 - $50,000,000' || item['amount'] == '$50,000,000 +'))
+    console.log(disclosures)
+    // disclosures.forEach(item => renderNavItem(item))
+})
 
-function populateNavigation
+// function renderNavItem(item){
+//     let navBar = document.querySelector('#navigationBar')
+//     let container = document.createElement('div')
+//     container.className = 'navItem'
+//     let date = document.createElement('h3')
+//     let member = document.createElement('h4')
+//     let ticker = document.createElement('h4')
 
-function renderDetails
+//     date.textContent = `Date: ${item.transaction_date}`
+//     member.textContent = `${item.representative}`
+//     ticker.textContent = `${item.ticker}`
 
-function 
+//     container.append(date, member,ticker)
+//     navBar.append(container)
+// }
 
 
 
+// function renderDetails()
+
+
+
+// function()
+
+
+// 
 
 // .amount categories
 // '$1,001 - $15,000'
